874d293d28c2419498d059cd5f8e637029f9ca4c
tsibikov <nortonts@yandex.ru>
2021-03-20 00:13:19 +0300

    add grabber

---
+++ grabber.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import feedparser
+
+
+class Grabber:
+    def __init__(self):
+        self.urls = [
+            os.environ.get('LENTA_URL'),
+            os.environ.get('INTERFAX_URL'),
+            os.environ.get('KOMMERSANT_URL'),
+            os.environ.get('M24_URL')
+        ] 
+
+    def get_news(self, channel, limit):
+        url = self.urls[channel]
+        feed = feedparser.parse(url)
+        all_data = feed["items"]
+        news = []
+        for event in all_data:
+            if limit > 0:
+                item = {}
+                item['title'] = event['title']
+                item['link'] = event['link']
+                item['text'] = event['description']
+                item['pub_date'] = event['published']
+                if channel == 0 or channel == 3:
+                    item['image'] = event['links'][1]['href']
+                news.append(item)
+                limit -= 1
+            else:    
+                return news                 
+
+    def get_all_news(self, limit):
+        all_news = []
+        for channel in range(0,4):
+            all_news.append(self.get_news(channel, limit))
+        return all_news
+
+
+
+if __name__ == '__main__':
+    limit = 3
+    channel = 3
+    grabber = Grabber()
+    #print(grabber.get_news(channel, limit))
+    print(grabber.get_all_news(limit))
